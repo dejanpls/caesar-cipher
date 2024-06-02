7f744cbf68c6f2dc42de5fbf578304fbf379380f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-06-02 14:27:15 +0200

    Implement a caesar cipher
    What was done:
    	- it takes in a string and the shift factor and then outputs the modified string

---
+++ caesar_cipher.rb
@@ -0,0 +1,24 @@
+def caesar_cipher(string, number)
+    lower_case_alpha = ('a'..'z').zip(0..25).to_h
+    upper_case_alpha = ('A'..'Z').zip(0..25).to_h
+    encrypted = string.split("").map do |letter|
+        if lower_case_alpha.include?(letter)
+            sub_letter = get_sub_letter(lower_case_alpha, letter, number)
+            letter.replace(sub_letter)
+        elsif upper_case_alpha.include?(letter)
+            sub_letter = get_sub_letter(upper_case_alpha, letter, number)
+            letter.replace(sub_letter)
+        else
+            letter
+        end
+    end
+    puts encrypted.join
+end
+
+def get_sub_letter(array, letter, number)
+    sub_idx = array[letter] + number
+    sub_idx = sub_idx - 26 if sub_idx > 26 # a way to wrap from "z" t "a"
+    array.key(sub_idx)    
+end
+
+caesar_cipher("What a StRing!", 5)
