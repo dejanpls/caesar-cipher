fa979cdce7315b315ce4c34191e8dce4315058aa
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-09-22 15:51:59 +0200

    Refactor a caesar cipher
        What was done:
                - it takes in a string and the shift factor and then outputs the modified string

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,24 +1,21 @@
 def caesar_cipher(string, number)
-    lower_case_alpha = ('a'..'z').zip(0..25).to_h
-    upper_case_alpha = ('A'..'Z').zip(0..25).to_h
-    encrypted = string.split("").map do |letter|
-        if lower_case_alpha.include?(letter)
-            sub_letter = get_sub_letter(lower_case_alpha, letter, number)
-            letter.replace(sub_letter)
-        elsif upper_case_alpha.include?(letter)
-            sub_letter = get_sub_letter(upper_case_alpha, letter, number)
-            letter.replace(sub_letter)
-        else
-            letter
-        end
-    end
-    puts encrypted.join
+  alpha = ("a".."z").to_a
+  alpha = alpha.reduce({}) do |hsh, i|
+    hsh[i] = alpha.index(i)
+    hsh
+  end 
+
+  array = string.split("")
+
+  array.map do |l|
+    i = alpha[l.downcase]
+    i = i.nil? ? nil : (i + number) % 26
+
+    new_letter = l == l.downcase ? alpha.key(i) : alpha.key(i).upcase
+    new_letter.nil? ? l : new_letter
+  end.join
 end
 
-def get_sub_letter(array, letter, number)
-    sub_idx = array[letter] + number
-    sub_idx -= 26 if sub_idx > 25 # a way to wrap from "z" to "a"
-    array.key(sub_idx)    
-end   
 
-caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 5)
+p caesar_cipher("What ", 515)
